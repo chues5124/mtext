a3d0794929af0604c07b64beb0aabacec54fd872
lydia <s1061641@gm.pu.edu.tw>
2018-11-23 12:11:33 +0800

    q1 q2 q3

---
+++ Assets/SQ.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SQ : MonoBehaviour {
+    public void Str() {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void End() {
+        Application.Quit();
+    }
+}
